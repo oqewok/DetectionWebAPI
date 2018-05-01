67e65ec364e7cc0426df55276eb59f92b0c53bcc
Andrey Okomin <oqewok@gmail.com>
2018-05-01 19:03:27 +0300

    Add TestController with specific route attribute

---
+++ src/DetectionAPI/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace DetectionAPI.Controllers
+{
+
+    [Route("api/test")]
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult JustMethod()
+        {
+            Console.WriteLine(Request.RequestUri);
+            var dr = "Test controller works";
+            Console.WriteLine(dr.ToString() + " " + DateTime.Now.ToString("HH:MM:SS.fff"));
+
+            if (dr == null)
+            {
+                //return StatusCode(HttpStatusCode.NotFound);
+                return NotFound();
+            }
+            return Ok(dr);
+        }
+
+
+    }
+}
