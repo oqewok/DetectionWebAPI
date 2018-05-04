635c82a1dd1d7aaa24857a05d78ffee6ef6cbcb0
Andrey Okomin <oqewok@gmail.com>
2018-05-04 22:05:09 +0300

    Read raw binary data (image)

--- src/DetectionAPI/Controllers/ImageUploadController.cs
+++ src/DetectionAPI/Controllers/ImageUploadController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -80,9 +81,73 @@ namespace DetectionAPI.Controllers
 
         }
 
+
+        /// <summary>
+        /// Read raw binary data (image)
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("api/imageupload/ReadBinaryImage")]
+        public async Task<IHttpActionResult> ReadFormData()
+        {
+            try
+            {
+                var result = await Request.Content.ReadAsByteArrayAsync();
+
+                using (var ms = new MemoryStream(result))
+                {
+                    MemoryStream memstr = new MemoryStream(result);
+
+                    Image img = Image.FromStream(memstr);
+                    ////or
+                    //Bitmap pic = new Bitmap(memstr);
+                    ////or
+                    //Bitmap correctedPic = GetImageFromByteArray(result);
+
+                    var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "DetectionAPI");
+                    Directory.CreateDirectory(path);
+
+                    //save all pics
+                    img.Save(path + "\\img" + Guid.NewGuid() + ".jpg");
+                    //pic.Save(path + "\\" + Guid.NewGuid() + ".jpg");
+                    //correctedPic.Save(path + "\\CORRECTED" + Guid.NewGuid() + ".jpg");
+                }
+            }
+
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+
+            return Ok();
+
+        }
+
         public class UploadedParameter
         {
             public string Some_text { get; set; }
+        }
+
+
+        /// <summary>
+        /// Function that converts from byte[] to Bitmap with correction of glitch of png files
+        /// </summary>
+        private static readonly ImageConverter _imageConverter = new ImageConverter();
+        public static Bitmap GetImageFromByteArray(byte[] byteArray)
+        {
+            Bitmap bm = (Bitmap)_imageConverter.ConvertFrom(byteArray);
+
+            if (bm != null && (bm.HorizontalResolution != (int)bm.HorizontalResolution ||
+                               bm.VerticalResolution != (int)bm.VerticalResolution))
+            {
+                // Correct a strange glitch that has been observed in the test program when converting 
+                //  from a PNG file image created by CopyImageToByteArray() - the dpi value "drifts" 
+                //  slightly away from the nominal integer value
+                bm.SetResolution((int)(bm.HorizontalResolution + 0.5f),
+                                 (int)(bm.VerticalResolution + 0.5f));
+            }
+
+            return bm;
         }
 
 
