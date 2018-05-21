4db3a29e759629e34478b3ab3abb262020b60e4b
Andrey Okomin <oqewok@gmail.com>
2018-05-21 17:35:33 +0300

    Added status controller

---
+++ src/DetectionAPI/Controllers/StatusController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Http;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace DetectionAPI.Controllers
+{
+    public class StatusController : ApiController
+    {
+        [HttpGet]
+        [Route("api/status")]
+        public IHttpActionResult Status()
+        {
+            var msg = new StatusMessage()
+            {
+                Status = "Service is up"
+            };
+
+            return Ok(msg);
+        }
+
+    }
+
+    [DataContract]
+    public class StatusMessage
+    {
+        [DataMember]
+        [JsonProperty(PropertyName = "status")]
+        public string Status { get; set; }
+
+    }
+}
